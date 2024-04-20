1cdf99192670cef850fd4f7eca9dbe483fa76618
Alexey Michurin <a.michurin@gmail.com>
2024-04-20 07:04:47 +0300

    Codecov API: retries

--- codecov/list.sh
+++ codecov/list.sh
@@ -1,3 +1,30 @@
-#!/bin/sh -ex
+#!/bin/sh
 
-curl -s https://codecov.io/api/gh/michurin --retry 10 | jq -r '.repos[].name' | sort >list.tmp
+for i in 1 2 3 4 5
+do
+    echo "Take $i..."
+    curl -qs https://codecov.io/api/gh/michurin --retry 10 | jq -r '.repos[].name' | sort >list.tmp
+    if test -s list.tmp
+    then
+        echo "list.tmp seems ok"
+        echo "===="
+        cat list.tmp
+        echo "==== EOF"
+        exit
+    else
+        echo "list.tmp is empty; we are going to sleep"
+        sleep 1
+    fi
+done
+
+echo "We are falling back :-("
+# fallback if codecov api is unreachable again :-(
+cat <<LIST >list.tmp
+human-readable-json-logging
+jsonpainter
+milisp
+minlog
+netpunch
+pprofiler
+systemd-env-file
+LIST
