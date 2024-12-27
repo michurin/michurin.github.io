709d5b242f5086959e604e98aa15fd834fe65cfe
Alexey Michurin <a.michurin@gmail.com>
2024-12-27 12:45:27 +0300

    Codecov: badge processing: cosmetics, comments, hints and instructions

--- codecov/cov.sh
+++ codecov/cov.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -ex
 
+# it can be useful to do
+# export https_proxy=socks5://localhost:1080
+
 output='api_codecov.json.tmp'
 
 echo '{' >"$output"
@@ -9,7 +12,7 @@ do
     echo "REPO: $repo"
     url="https://codecov.io/gh/michurin/$repo/branch/master/graph/badge.svg"
     echo "URL: $url"
-    curl -s "$url" |
+    curl -qs "$url" |
         tee "badge.$repo.svg.tmp" |
         perl -0777 -e '$_=<>; m/<text[^>]*>(\d{,3}%)</; print(q|'$sep'"'$repo'":"|.$1.q|"|);' >>"$output" # TODO use XSLT to extract coverage?
     sep=','
