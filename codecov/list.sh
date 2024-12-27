f2b04f5d57d47423646b7cdc7dc43147a735b5ee
Alexey Michurin <a.michurin@gmail.com>
2024-12-27 12:47:22 +0300

    Codecov: list repos: it seems codecov.io/api wont work anymore

--- codecov/list.sh
+++ codecov/list.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-for i in 1 2 3 4 5
-do
-    echo "Take $i..."
-    curl -qs https://codecov.io/api/gh/michurin --retry 10 | jq -r '.repos[].name' | sort >list.tmp
-    if test -s list.tmp
-    then
-        echo "list.tmp seems ok"
-        echo "===="
-        cat list.tmp
-        echo "==== EOF"
-        exit
-    else
-        echo "list.tmp is empty; we are going to sleep"
-        sleep 1
-    fi
-done
+# It seems it doesn't work anymore
+#
+# for i in 1 2 3 4 5
+# do
+#     echo "Take $i..."
+#     curl -qs https://codecov.io/api/gh/michurin --retry 10 | jq -r '.repos[].name' | sort >list.tmp
+#     if test -s list.tmp
+#     then
+#         echo "list.tmp seems ok"
+#         echo "===="
+#         cat list.tmp
+#         echo "==== EOF"
+#         exit
+#     else
+#         echo "list.tmp is empty; we are going to sleep"
+#         sleep 1
+#     fi
+# done
 
-echo "We are falling back :-("
+#echo "We are falling back :-("
 # fallback if codecov api is unreachable again :-(
 cat <<LIST >list.tmp
 cnbot
@@ -26,6 +28,7 @@ jsonpainter
 milisp
 minlog
 netpunch
+ngramindex
 pprofiler
 systemd-env-file
 LIST
